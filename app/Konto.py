76c3cadead7730a25c2653c485e8126e7ae9520a
kalisowski <lisekgda3@gmail.com>
2022-10-24 23:32:45 +0200

    WIP

--- app/Konto.py
+++ app/Konto.py
@@ -1,5 +1,27 @@
 class Konto:
-    def __init__(self, imie, nazwisko):
+    def __init__(self, imie, nazwisko, pesel, prom=None):
         self.imie = imie
         self.nazwisko = nazwisko
-        self.saldo = 0
+        self.pesel = self.pesel_check(pesel)
+        self.prom = prom
+        self.saldo = self.prom_check()
+
+    def pesel_check(self, pesel):
+        if (len(pesel) != 11):
+            return "Niepoprawny pesel!"
+        else:
+            return pesel
+
+    def prom_check(self):
+        if self.birth_check() and self.prom != None:
+            if len(self.prom) == 8:
+                if self.prom[0:5] == "PROM_":
+                    return 50
+        return 0
+
+    def birth_check(self):
+        pesel = self.pesel
+        if pesel != "Niepoprawny pesel!":
+            if (int(pesel[2]) > 1 or (int(pesel[0]) >= 6 and int(pesel[2]) < 2)):
+                return True
+        return False
--- app/tests/test_create_bank_account.py
+++ app/tests/test_create_bank_account.py
@@ -1,13 +1,35 @@
 import unittest
-
+import re
 from ..Konto import Konto
+
 
 class TestCreateBankAccount(unittest.TestCase):
 
     def test_tworzenie_konta(self):
-        pierwsze_konto = Konto("Dariusz", "Januszewski")
-        self.assertEqual(pierwsze_konto.imie, "Dariusz", "Imie nie zostało zapisane!")
-        self.assertEqual(pierwsze_konto.nazwisko, "Januszewski", "Nazwisko nie zostało zapisane!")
+        pierwsze_konto = Konto("Dariusz", "Januszewski", "01234567891")
+        self.assertEqual(pierwsze_konto.imie, "Dariusz",
+                         "Imie nie zostało zapisane!")
+        self.assertEqual(pierwsze_konto.nazwisko, "Januszewski",
+                         "Nazwisko nie zostało zapisane!")
         self.assertEqual(pierwsze_konto.saldo, 0, "Saldo nie jest zerowe!")
+        self.assertEqual(pierwsze_konto.pesel,
+                         '01234567891', "Pesel nie został zapisany!")
 
-    #tutaj proszę dodawać nowe testy
+    def test_tworzenie_kont2(self):
+        drugie_konto = Konto("Dariusz", "Januszewski",
+                             "0123456789", "PROM_331")
+        print(drugie_konto.saldo)
+        self.assertEqual(drugie_konto.pesel,
+                         "Niepoprawny pesel!", "Podano zły pesel")
+        self.assertEqual(drugie_konto.prom, "PROM_331", "Podano zły format")
+
+    def test_brak_promo(self):
+        drugie_konto = Konto("Dariusz", "Januszewski",
+                             "0123456789")
+        self.assertEqual(drugie_konto.prom, None, "Podano zły format")
+
+    def test_kod_zbyt_stary(self):
+        konto_za_stary = Konto(
+            "Jan", "Kowalski", "52111461823", "PROM_332")
+        print(konto_za_stary.birth_check())
+        self.assertEqual(konto_za_stary.birth_check(), False)
