2837c6537422d952ac677f33d98a0bb0101db612
kalisowski <lisekgda3@gmail.com>
2022-10-26 19:12:01 +0200

    done

--- app/tests/test_create_bank_account.py
+++ app/tests/test_create_bank_account.py
@@ -1,5 +1,4 @@
 import unittest
-import re
 from ..Konto import Konto
 
 
@@ -18,18 +17,55 @@ class TestCreateBankAccount(unittest.TestCase):
     def test_tworzenie_kont2(self):
         drugie_konto = Konto("Dariusz", "Januszewski",
                              "0123456789", "PROM_331")
-        print(drugie_konto.saldo)
         self.assertEqual(drugie_konto.pesel,
                          "Niepoprawny pesel!", "Podano zły pesel")
-        self.assertEqual(drugie_konto.prom, "PROM_331", "Podano zły format")
+        self.assertEqual(drugie_konto.prom, "PROM_331",
+                         "Podano zły format kodu")
 
     def test_brak_promo(self):
-        drugie_konto = Konto("Dariusz", "Januszewski",
-                             "0123456789")
-        self.assertEqual(drugie_konto.prom, None, "Podano zły format")
+        konto_brak_promo = Konto("Dariusz", "Januszewski",
+                                 "0123456789")
+        self.assertEqual(konto_brak_promo.prom, None,
+                         "Konto nie powinno mieć domyślnie promo")
 
-    def test_kod_zbyt_stary(self):
-        konto_za_stary = Konto(
+    def test_złykod_wiek_po60ur(self):
+        konto_złykod_wiek_po60ur = Konto(
+            "Jan", "Kowalski", "75021456785", "APROM_33")
+        self.assertEqual(konto_złykod_wiek_po60ur.birth_check(), True,
+                         "Wiek powinien być poprawny!")
+        self.assertEqual(konto_złykod_wiek_po60ur.saldo, 0,
+                         "Saldo powinno wynosić 0!")
+        self.assertEqual(konto_złykod_wiek_po60ur.prom_check(),
+                         0, "Saldo powinno wynosić 0")
+
+    def test_złykod_wiek_zastary(self):
+        konto_złykod_wiek_zastary = Konto(
+            "Jan", "Kowalski", "52021456785", "APROM_33")
+        self.assertEqual(konto_złykod_wiek_zastary.birth_check(), False,
+                         "Wiek powinien być poprawny!")
+        self.assertEqual(konto_złykod_wiek_zastary.saldo, 0,
+                         "Saldo powinno wynosić 0!")
+        self.assertEqual(konto_złykod_wiek_zastary.prom_check(),
+                         0, "Saldo powinno wynosić 0")
+
+    def test_kod_wiek_po60ur(self):
+        konto_kod_wiek_po60ur = Konto(
+            "Jan", "Kowalski", "75021456785", "PROM_332")
+        self.assertEqual(konto_kod_wiek_po60ur.birth_check(), True,
+                         "Wiek powinien być poprawny!")
+        self.assertEqual(konto_kod_wiek_po60ur.saldo, 50,
+                         "Saldo powinno wynosić 50!")
+        self.assertEqual(konto_kod_wiek_po60ur.prom_check(),
+                         50, "Saldo powinno wynosić 50")
+
+    def test_kod_wiek_zastary(self):
+        konto_kod_wiek_zastary = Konto(
             "Jan", "Kowalski", "52111461823", "PROM_332")
-        print(konto_za_stary.birth_check())
-        self.assertEqual(konto_za_stary.birth_check(), False)
+        self.assertEqual(konto_kod_wiek_zastary.prom, "PROM_332",
+                         "Kod promocyjny nie został poprawnie wprowadzony")
+        self.assertEqual(konto_kod_wiek_zastary.birth_check(), False,
+                         "Wiek nie powinien być poprawny!")
+        self.assertEqual(konto_kod_wiek_zastary.saldo,
+                         0, "Saldo powinno wynosić 0!")
+        self.assertEqual(konto_kod_wiek_zastary.prom_check(),
+                         0, "Saldo powinno wynosić 0")
